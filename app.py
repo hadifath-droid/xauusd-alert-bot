f83f4d1e64c4bc9ba270a039282ebaa92f3cb5ba
hadifath-droid <prisma.raharjo@gmail.com>
2026-06-14 10:59:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import os
 import requests
 
@@ -24,6 +24,22 @@ def send_test():
     requests.post(url, json=payload)
 
     return "Message Sent"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+
+    data = request.json
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": f"📈 TradingView Alert\n\n{data}"
+    }
+
+    requests.post(url, json=payload)
+
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
