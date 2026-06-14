5a737d8906e1e4feef83f6416741135d6af0688c
hadifath-droid <prisma.raharjo@gmail.com>
2026-06-14 10:45:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask
+import os
+import requests
 
 app = Flask(__name__)
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 @app.route("/")
 def home():
     return "XAUUSD Alert Bot Running"
+
+@app.route("/send-test")
+def send_test():
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": "🚀 TEST SUCCESS\n\nRailway berhasil kirim pesan ke Telegram!"
+    }
+
+    requests.post(url, json=payload)
+
+    return "Message Sent"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
